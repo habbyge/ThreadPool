1a5479c8c92016a60b36e6fe0c70c691812b01f3
habbyge <habbyge@tencent.com>
2021-05-31 19:48:24 +0800

    分析

--- thread_pool.h
+++ thread_pool.h
@@ -66,8 +66,8 @@ auto ThreadPool::enqueue(F&& f, Args&& ... args) -> std::future<typename std::re
 
   using return_type = typename std::result_of<F(Args...)>::type;
 
-  auto task = std::make_shared < std::packaged_task < return_type >> (std::bind(
-      std::forward<F>(f), std::forward<Args>(args)...));
+  auto task = std::make_shared <std::packaged_task<return_type>>(
+      std::bind(std::forward<F>(f), std::forward<Args>(args)...));
 
   std::future <return_type> result = task->get_future();
 
